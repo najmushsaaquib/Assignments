c7da8c73199b3eb7916c6f70389dbbfe55c3ad99
Najmush Saaquib <saaquibnajmush@gmail.com>
2022-03-02 19:46:24 +0530

    Solved

--- Day 1/scripts/display.js
+++ Day 1/scripts/display.js
@@ -1,1 +1,20 @@
 //here retrieve the generated number from Localstorage.
+
+let numberArr = JSON.parse(localStorage.getItem("number"));
+
+console.log(numberArr);
+
+let data = numberArr[0].number;
+
+console.log(data);
+
+let show = document.querySelector("h1")
+show.innerText = data;
+
+if (data === 6) {
+  show.style.color = "green";
+} else if (data === 1) {
+  show.style.color = "yellow";
+} else {
+  show.style.color = "red";
+}
--- Day 1/scripts/index.js
+++ Day 1/scripts/index.js
@@ -1,1 +1,23 @@
 //Store the generated number in LocalStorage with key "number".
+
+let dice = document.createElement("button");
+dice.innerText = "Throw Me";
+dice.setAttribute("id", "throw_dice");
+document.body.append(dice);
+
+document.querySelector("#throw_dice").addEventListener("click", throwMe);
+console.log("I am workng");
+
+let dataArr = [];
+
+function throwMe() {
+  console.log(Math.floor(Math.random() * 6));
+  let num = {
+    number: Math.floor(Math.random() * 6) + 1,
+  };
+
+  dataArr.push(num);
+  localStorage.setItem("number", JSON.stringify(dataArr));
+
+  window.location.href = "/display.html";
+}
