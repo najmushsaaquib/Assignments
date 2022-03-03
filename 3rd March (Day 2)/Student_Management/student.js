5cf77125e19052e155376cb1b699debdb4bddf95
Najmush Saaquib <saaquibnajmush@gmail.com>
2022-03-03 21:44:14 +0530

    changes made

---
+++ 3rd March (Day 2)/Student_Management/student.js
@@ -0,0 +1,33 @@
+
+document.querySelector('#myForm').addEventListener("submit", formSubmit)
+
+function createObj(name,num,city){
+
+    this.name = name
+    this.number = num
+    this.city = city
+}
+
+
+let arrData = JSON.parse(localStorage.getItem("studentData")) || []
+
+console.log(arrData)
+
+function formSubmit(event){
+    event.preventDefault()
+
+    let name = document.querySelector("#name").value
+    let number = document.querySelector("#number").value
+    let city = document.querySelector("#city").value
+
+    let myObj = new createObj(name,number,city)
+
+    arrData.push(myObj)
+
+    localStorage.setItem("studentData", JSON.stringify(arrData))
+
+    document.querySelector("#name").value = null
+    document.querySelector("#number").value = null
+    document.querySelector("#city").value = null
+
+}
