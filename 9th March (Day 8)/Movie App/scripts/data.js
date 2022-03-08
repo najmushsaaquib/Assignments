fd18e96f03d6def473999f8d30ba5e7fc207bc69
Najmush Saaquib <saaquibnajmush@gmail.com>
2022-03-08 23:59:34 +0530

    'done

---
+++ 9th March (Day 8)/Movie App/scripts/data.js
@@ -0,0 +1,64 @@
+let dataArr = [
+  {
+    img: "https://wallpaperaccess.com/full/1632109.jpg",
+    name: "The Shawshank Redemption",
+    genre: "Drama",
+    rating: 9.4,
+  },
+  {
+    img: "https://w0.peakpx.com/wallpaper/557/513/HD-wallpaper-the-godfather-marlon-brando-vito-corleone.jpg",
+    name: "The Godfather",
+    genre: "Drama, Crime",
+    rating: 8.6,
+  },
+
+  {
+    img: "https://wallpaperaccess.com/full/4594643.jpg",
+    name: "The Dark Knight",
+    genre: "Drama, Crime, Action",
+    rating: 9.0,
+  },
+
+  {
+    img: "https://wallpaperaccess.com/full/6790561.jpg",
+    name: "Spider-Man: No Way Home",
+    genre: "Drama, Crime",
+    rating: 9.2,
+  },
+
+  {
+    img: "https://cdn.wallpapersafari.com/37/38/AYBiND.jpg",
+    name: "The Angry Men",
+    genre: "Drama, Crime",
+    rating: 6.7,
+  },
+
+  {
+    img: "https://themarckoguy.files.wordpress.com/2015/08/movie_classical_schindler_s_list_hands_movie_123903_1366x768.jpg",
+    name: "Schindler's List",
+    genre: "Drama, Biography, History",
+    rating: 7.9,
+  },
+
+  {
+    img: "https://wallpaperaccess.com/full/160477.jpg",
+    name: "The Lord of The Rings",
+    genre: "Drama, Action, Adventure",
+    rating: 7.3,
+  },
+
+  {
+    img: "https://www.nolanfans.com/wordpress/wp-content/uploads/2010/06/inception_new_banner2_large.jpg",
+    name: "Inception",
+    genre: "Drama,Thriller, Sci-Fi, Action",
+    rating: 8.8,
+  },
+
+  {
+    img: "https://cdn.wallpapersafari.com/97/65/v49Dqr.jpg",
+    name: "Interstellar",
+    genre: "Adventure, Drama, Sci-fi",
+    rating: 9.0,
+  },
+];
+localStorage.setItem("movies", JSON.stringify(dataArr));
