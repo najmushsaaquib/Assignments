73bf48e08f6dfa8dbb6699aef52c1959a5e9866e
Najmush Saaquib <saaquibnajmush@gmail.com>
2022-03-17 00:30:24 +0530

    'changes'

--- 15th March(Day 13)/YouTube/main.js
+++ 15th March(Day 13)/YouTube/main.js
@@ -1,31 +1,27 @@
-    // Youtube API - Reading document
+// Youtube API - Reading document
 
-    // collecting Array of Object
+// collecting Array of Object
 
-    // Appending the Data in DOM
+// Appending the Data in DOM
 
-    // My API Key : AIzaSyDTr9EoUscFUXB6t-8AT76A1-bUam3C_iQ
+// My API Key : AIzaSyDTr9EoUscFUXB6t-8AT76A1-bUam3C_iQ
 
-    //https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=standup%20comedy&key=[YOUR_API_KEY]
+//https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=standup%20comedy&key=[YOUR_API_KEY]
 
+// ! Possible mistakes
+//1. async await not in pair
+// 2. https
+// 3. res.json()
+// 4. seperate parameters using &
+// 5. wrong destination
+// 6. spelling mistake
 
-    // ! Possible mistakes
-    //1. async await not in pair
-    // 2. https
-    // 3. res.json()
-    // 4. seperate parameters using &
-    // 5. wrong destination
-    // 6. spelling mistake
+// ! making function to append data
 
-    // ! making function to append data
+// we want to embed our video to our app - means we want to add video to our app -
+// embed - <iframe width="560" height="315" src="https://www.youtube.com/embed/90JsrQwE5CA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 
-    // we want to embed our video to our app - means we want to add video to our app -
-    // embed - <iframe width="560" height="315" src="https://www.youtube.com/embed/90JsrQwE5CA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-
-    // embed url - src="https://www.youtube.com/embed/90JsrQwE5CA"
-
- 
-
+// embed url - src="https://www.youtube.com/embed/90JsrQwE5CA"
 
 const resultDiv = document.getElementById("results");
 
@@ -49,24 +45,32 @@ const searchVideos = async () => {
   }
 };
 
-
 const appendVideos = (videoArr) => {
-    resultDiv.innerHTML = null;
+  resultDiv.innerHTML = null;
 
-    videoArr.forEach(({ snippet, id: videoId }) => {
+  videoArr.forEach(
+    ({
+      snippet,
+      snippet: {
+        thumbnails: {
+          high: { url },
+        },
+      },
+    }) => {
       let main = document.createElement("div");
       main.setAttribute("id", "wrapper");
 
       let title = document.createElement("p");
       title.textContent = snippet.title;
 
-      let iframe = document.createElement("iframe");
-      iframe.src = `https://www.youtube.com/embed/${videoId}`;
-      iframe.height = "100%";
-      iframe.width = "100%";
+      let iframe = document.createElement("img");
+      iframe.src = url;
+      iframe.style.height = "195px";
+      iframe.style.width = "295px";
 
       main.append(iframe, title);
 
       resultDiv.append(main);
-    });
-  };
+    }
+  );
+};
