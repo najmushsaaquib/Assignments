734da45139e06d2d25102009f31aa35587e2da29
najmushsaaquib <saaquibnajmush@gmail.com>
2022-03-26 00:03:54 +0530

    first node module

---
+++ 24th March(Day 20)/script.js
@@ -0,0 +1,21 @@
+console.log("Hello");
+
+import { v4 as uuidv4 } from "uuid";
+
+let user1 = {
+  name: "Najmush",
+  id: uuidv4(),
+};
+
+let user2 = {
+  name: "Saaquib",
+  id: uuidv4(),
+};
+
+let user3 = {
+    name: "Rahul",
+    id: uuidv4(),
+};
+
+
+console.log(user3)
